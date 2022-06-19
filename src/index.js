12594e197aea57da46ff6e84375546f43cbdc2a0
Candi Agusta <mugiwaraagusta@gmail.com>
2022-06-19 23:12:14 +0700

    init

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  Login,
+  Dashboard,
+  Destinations,
+  Events,
+  Account,
+  AddDestination,
+  UpdateDestination,
+  AddEvent,
+  DestinationDetail,
+  Order,
+  UpdateEvent,
+  EventDetail,
+  OrderDetail,
+  Main,
+} from "./pages";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/destinations" element={<Destinations />} />
+      <Route path="/destinations/:id" element={<DestinationDetail />} />
+      <Route path="/destinations/add" element={<AddDestination />} />
+      <Route path="/destinations/update/:id" element={<UpdateDestination />} />
+      <Route path="/events" element={<Events />} />
+      <Route path="/events/:id" element={<EventDetail />} />
+      <Route path="/events/add" element={<AddEvent />} />
+      <Route path="/events/update/:id" element={<UpdateEvent />} />
+      <Route path="/orders" element={<Order />} />
+      <Route path="/orders/:id" element={<OrderDetail />} />
+      <Route path="/users" element={<Account />} />
+    </Routes>
+  </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
